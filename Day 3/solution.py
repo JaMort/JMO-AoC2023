bbf9986c55768a8c8e60eef66df9da175ef2267a
Jakob Ole Mortensen <j.mortensen1996@outlook.dk>
2023-12-06 19:56:16 +0100

    Solution for day 3 part 1

--- Day 3/solution.py
+++ Day 3/solution.py
@@ -5,30 +5,26 @@ input = open('input.txt', 'r')
 lines = input.read().splitlines()
 input.close()
 
-# part 1 - find the sum of all engine numbers that are adjacent (even diagonally) to a symbol
-part_numbers = []
-for i, line in enumerate(lines):
-    line_above = None
-    line_below = None
-    if i != 0:
-        line_above = lines[i-1]
-    if i != len(lines) -1:
-        line_below = lines[i+1]
-    numbers_in_line = [int(s) for s in re.findall(r'\d+', line)]
-    for number in numbers_in_line:
-        adjacent = ''
-        index = line.index(str(number))
-        index_end = index + len(str(number)) + 3 if index == 0 else index + len(str(number))
-        if index > 0:
-            index = index-1
-        if index_end < len(line)-1:
-            index_end = index_end+1
-        if line_above != None:
-            adjacent += line_above[index: index_end]
-        if line_below != None:
-            adjacent += line_below[index: index_end]
-        adjacent += line[index: index_end]
-        if any(n != '.' for n in adjacent):
-            part_numbers.append(number)
+def get_surrounding(matrix, row, col):
+    surroundings = []
+    if 1 < row:
+        surroundings += matrix[row-1][max(0, col[0] - 1):min(col[1]+1, len(matrix[0]))]
+    if row < len(matrix) - 1:
+        surroundings += matrix[row + 1][max(0, col[0] - 1):min((col[1] + 1, len(matrix[0])))]
+    surroundings += matrix[row][max(0, col[0]-1):col[0]]
+    surroundings += matrix[row][col[1]:min(col[1] + 1, len(matrix[0]))]
 
-print(sum(part_numbers))
+    return surroundings
+
+
+# part 1 - find the sum of all engine numbers that are adjacent (even diagonally) to a symbol
+sum = 0
+for r, line in enumerate(lines):
+    for number in re.finditer(r'\d+', line):
+        n = int(number.group())
+        s = get_surrounding(lines, r, number.span())
+        s = ''.join(s)
+        if re.search(r'[^0-9.]', s):
+            sum += n
+
+print(sum)
